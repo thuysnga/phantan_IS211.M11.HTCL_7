40c459dd493fb76e82fdf5b12806b507e511bd52
ThuysNga <19521881@gm.uit.edu.vn>
2021-12-10 09:45:18 +0700

    trigger

--- BTL1/trigger.sql
+++ BTL1/trigger.sql
@@ -1,2 +1,33 @@
-CREATE OR REPLACE TRIGGER trg_soluongsach
-AFTER 
+CREATE OR REPLACE TRIGGER trgg_cthd_soluongkho
+BEFORE INSERT ON CN1.CTHD
+FOR EACH ROW
+DECLARE
+    v_sl int;
+BEGIN
+    SELECT SoLuong
+    INTO v_sl
+    FROM cn1.KHOSANPHAM_QLKHO
+    WHERE MASP = :NEW.MASP;
+
+    IF :NEW.SoLuong > v_sl THEN
+    BEGIN
+        RAISE_APPLICATION_ERROR(-20001,'So trong kho khong du');
+        ROLLBACK;
+    END;
+    ELSE
+    BEGIN
+        UPDATE cn1.KHOSANPHAM_QLKHO
+        SET SoLuong = SoLuong - v_sl
+        WHERE MASP = :NEW.MASP;
+
+        IF :NEW.SoLuong = v_sl THEN
+            UPDATE cn1.KHOSANPHAM_QLBH
+            SET TinhTrang = 'Het Hang'
+            WHERE MASP = :NEW.MASP;
+        END IF;
+
+    END;
+    END IF;
+
+END;
+/
