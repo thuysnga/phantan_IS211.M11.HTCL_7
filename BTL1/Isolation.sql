2a39a4dba56cc63822337349d6cb53db24b88545
ThuysNga <19521881@gm.uit.edu.vn>
2021-12-13 15:36:11 +0700

    edit

--- BTL1/Isolation.sql
+++ BTL1/Isolation.sql
@@ -1,5 +1,6 @@
-
 SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+
 alter session set ISOLATION_LEVEL= SERIALIZABLE;
 alter session set ISOLATION_LEVEL= READ COMMITTED;
 --mức cô lập hiện tại
@@ -8,6 +9,7 @@ trans_id varchar(100);
 begin
 trans_id:= dbms_transaction.local_transaction_id(TRUE);
 end;
+/
 
 --Hiện
 SELECT s.sid, s.serial#,
@@ -20,34 +22,54 @@ JOIN v$session s ON t.addr = s.taddr;
 
 ---nonrepeatable read
 --- máy 1:
-select * from SANPHAM where MASP='TV05';
+select * from CN2.SANPHAM@cn2_dblink where MASP='TV05';
 
 ---máy 2:
- update SANPHAM 
- set Gia = 10000;
- where MASP='TV05';
+update CN2.SANPHAM 
+set Gia = 10000;
+where MASP='TV05';
 
- ---phantom read
+commit;
+
+---phantom read
 ---máy 1:
-INSERT INTO cn2.SANPHAM@... VALUES ('ST08','PHAN MAU KHONG BUI','HOP','VIETNAM',9000);
-INSERT INTO cn1.SANPHAM VALUES ('ST08','PHAN MAU KHONG BUI','HOP','VIETNAM',9000);
+INSERT INTO cn2.SANPHAM@cn2_dblink VALUES ('ST08','PHAN MAU KHONG BUI','HOP','VIETNAM',9000);
+commit;
+
 ---máy 2:
-SELECT * from cn2.sanpham;
+SELECT * from cn2.sanpham where MASP = 'ST08';
+
+---máy 1:
+INSERT INTO cn2.SANPHAM@cn2_dblink VALUES ('ST09','PHAN MAU KHONG BUI','HOP','VIETNAM',9000);
+commit;
+
+---máy 2:
+SELECT * from cn2.sanpham where MASP = 'ST09';
 
 ---lost update
 ---máy 1:
-update SANPHAM 
- set Gia = 10000;
- where MASP='TV05';
+select * from cn2.sanpham@cn2_dblink where MASP='TV05';
 
- ---máy 2:
-update SANPHAM 
- set Gia = 20000;
- where MASP='TV05';
+update CN2.SANPHAM@cn2_dblink
+set Gia = 10000
+where MASP='TV05';
 
- ---deadlock
+---máy 2:
+update CN2.SANPHAM
+set Gia = 20000
+where MASP='TV05';
+commit;
 
- ---máy 1:
+-- máy 1
+commit;
+
+-- máy 2
+select * from cn2.sanpham where MASP='TV05';
+
+
+---deadlock
+
+---máy 1:
 update SANPHAM 
  set Gia = 40000;
  where MASP='TV05';
